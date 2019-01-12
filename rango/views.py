a8dda080711f946242331b2597dd1805472be767
James Bradford <2324024b@student.gla.ac.uk>
2019-01-12 20:34:21 +0000

    Chapter 4 and exercises complete (templates, static, media parsing)

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    # Create key/value pairs for passing to template.
+    # in this case, boldmessage corresponds to a {{ boldmessage }} in index.html.
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Now return a rendered response, with the intial request,
+    # the template, and the context dict above
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango'>Home</a>")
+    context_dict = {'boldmessage': "This tutorial has been put together by James"}
+    return render(request, 'rango/about.html', context=context_dict)
